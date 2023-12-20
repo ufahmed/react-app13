e207aad8bbfb11daee0c1d5b84ce0215f55bfda8
ufahmed <ahmedsellami375@gmail.com>
2023-12-20 18:38:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import './App.css'; // You can create or import CSS for styling
 
-function App() {
+const App = () => {
+  const [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      try {
+        const response = await Axios.get('https://pokeapi.co/api/v2/pokemon?limit=807');
+        setPokemonList(response.data.results);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchPokemon();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>List of Pokemon</h1>
+      <ul>
+        {pokemonList.map((pokemon, index) => (
+          <li key={index}>{pokemon.name}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
